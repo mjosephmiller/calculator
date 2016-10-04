ad8b4a883def8dfe46a1302cfc229bde0601aa17
Mikey <mjosephmiller.uk@gmail.com>
2016-10-04 23:50:43 +0100

    refactored tests and adds a substraction feature

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,29 +2,36 @@ require './calculator'
 
 describe Calculator do
 
+  subject(:calc){described_class.new}
+
   describe "#add" do
+
     it 'should return the sum of two numbers' do
-      calc = Calculator.new
       expect(calc.add(2, 10)).to eq(12)
     end
 
     it 'should return the sum of another two numbers' do
-      calc = Calculator.new
       expect(calc.add(2, 6)).to eq(8)
     end
 
   end
 
+  describe "#subtract" do
+    it 'should subtract one number from another' do
+      expect(calc.subtract(2, 10)).to eq(-8)
+    end
+  end
+
   describe '#factorial' do
+
     it 'should return the factorial of a number' do
-      calc = Calculator.new
       expect(calc.factorial(0)).to eq(1)
     end
 
     it 'should return the factorial of a number' do
-      calc = Calculator.new
       expect(calc.factorial(5)).to eq(120)
     end
+
   end
 
 end
