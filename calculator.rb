a37e9ad25ba4ba22ccba8b71d4beb996bd511c0e
Mikey <mjosephmiller.uk@gmail.com>
2016-10-04 23:34:03 +0100

    adds factorial feature

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,13 @@ class Calculator
   def add(a, b)
     a+b
   end
+
+  def factorial(n)
+    if n < 1
+      1
+    else
+      n * factorial(n-1)
+    end
+  end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,6 +12,19 @@ describe Calculator do
       calc = Calculator.new
       expect(calc.add(2, 6)).to eq(8)
     end
+
+  end
+
+  describe '#factorial' do
+    it 'should return the factorial of a number' do
+      calc = Calculator.new
+      expect(calc.factorial(0)).to eq(1)
+    end
+
+    it 'should return the factorial of a number' do
+      calc = Calculator.new
+      expect(calc.factorial(5)).to eq(120)
+    end
   end
 
 end
